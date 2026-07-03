283a8781d46eb2c90226ba5b85310481d36dd861
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-07-03 16:05:28 -0700

    Adding helper to read directories.txt

--- new.go
+++ new.go
@@ -1,8 +1,12 @@
 package camino
 
 import (
+	"bufio"
+	"errors"
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 )
 
 type (
@@ -11,62 +15,102 @@ type (
 )
 
 var (
-	relParentPath map[RelPath]AbsPath
-	relPaths      map[RelPath]string
-	absPaths      map[AbsPath]string
+	absPaths    map[AbsPath]string
+	relPaths    map[RelPath]string
+	relAbsPaths map[RelPath][]AbsPath
 )
 
-func Register(ap AbsPath, absPath string, rps map[RelPath]string) error {
-	if absPaths == nil {
-		absPaths = make(map[AbsPath]string)
-	}
+func Register(absolutePaths map[AbsPath]string, relativePaths map[RelPath]string, relativeAbsolutePaths map[RelPath][]AbsPath) error {
 
-	absPaths[ap] = absPath
+	absPaths = absolutePaths
+	relPaths = relativePaths
+	relAbsPaths = relativeAbsolutePaths
 
-	if _, err := os.Stat(absPath); os.IsNotExist(err) {
-		if err = os.MkdirAll(absPath, DefaultFileMode); err != nil {
-			return err
-		}
-	}
-
-	if relParentPath == nil {
-		relParentPath = make(map[RelPath]AbsPath)
-	}
-
-	if relPaths == nil {
-		relPaths = make(map[RelPath]string)
-	}
-
-	for rp, relPath := range rps {
-		relPaths[rp] = relPath
-		relParentPath[rp] = ap
-
-		absRelPath := filepath.Join(absPath, relPath)
-
-		if _, err := os.Stat(absRelPath); os.IsNotExist(err) {
-			if err = os.MkdirAll(absRelPath, DefaultFileMode); err != nil {
+	for _, ap := range absolutePaths {
+		if _, err := os.Stat(ap); os.IsNotExist(err) {
+			if err = os.MkdirAll(ap, DefaultFileMode); err != nil {
 				return err
 			}
 		}
 	}
 
+	for rp, relPath := range relativePaths {
+
+		if aps, ok := relativeAbsolutePaths[rp]; ok && len(aps) > 0 {
+
+			for _, ap := range aps {
+				if absPath, sure := absolutePaths[ap]; sure {
+
+					absRelPath := filepath.Join(absPath, relPath)
+
+					if _, err := os.Stat(absRelPath); os.IsNotExist(err) {
+						if err = os.MkdirAll(absRelPath, DefaultFileMode); err != nil {
+							return err
+						}
+					}
+
+				} else {
+					return errors.New("unknown absolute path in relative absolute paths")
+				}
+			}
+
+		} else {
+			return errors.New("relative absolute paths not set")
+		}
+
+	}
+
 	return nil
 }
 
-func GetAbs(ap AbsPath) string {
-	if absPath, ok := absPaths[ap]; ok {
-		return absPath
+func GetAbs(absolutePath AbsPath) string {
+	if ap, ok := absPaths[absolutePath]; ok {
+		return ap
 	}
 
 	panic("abs path not registered")
 }
 
-func GetRel(rp RelPath) string {
-	if ap, ok := relParentPath[rp]; ok {
-		if rpp, sure := relPaths[rp]; sure {
-			return filepath.Join(GetAbs(ap), rpp)
+func GetRel(relativePath RelPath, absolutePath AbsPath) string {
+
+	if aps, ok := relAbsPaths[relativePath]; ok {
+		if !slices.Contains(aps, absolutePath) {
+			panic("rel path not registered under abs path")
+		}
+
+		if relPath, sure := relPaths[relativePath]; sure {
+			return filepath.Join(GetAbs(absolutePath), relPath)
+		}
+
+		panic("rel path not registered")
+	}
+
+	panic("abs paths not registered for rel path")
+}
+
+func ReadDirectories(path string) (map[string]string, error) {
+
+	directoriesFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer directoriesFile.Close()
+
+	directories := make(map[string]string)
+
+	scanner := bufio.NewScanner(directoriesFile)
+	for scanner.Scan() {
+
+		line := scanner.Text()
+
+		if absDir, absDirPath, ok := strings.Cut(line, "="); ok {
+			directories[absDir] = absDirPath
 		}
 	}
 
-	panic("rel path not registered")
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return directories, nil
 }
